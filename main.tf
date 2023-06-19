a0dc93ab3a5bb5e96d4e10975faac78cd8e483cb
root <root@ip-172-31-55-228.ec2.internal>
2023-06-19 19:20:42 +0000

    this is a jenkins terraform project

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAZMQH2CGRQMQW3DH7"
+  secret_key = "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"
+}
+
+resource "aws_s3_bucket" "one" {
+   bucket = "s3project1"
+}
+
